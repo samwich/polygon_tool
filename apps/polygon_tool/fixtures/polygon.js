c5a3df8efdecbdee87309b05dd049402f98b09ff
Sam Johnson <sam@samwich.com>
2009-10-10 20:26:14 -0700

    sc-gen model PolygonTool.Polygon

---
+++ apps/polygon_tool/fixtures/polygon.js
@@ -0,0 +1,35 @@
+// ==========================================================================
+// Project:   PolygonTool.Polygon Fixtures
+// Copyright: ©2009 My Company, Inc.
+// ==========================================================================
+/*globals PolygonTool */
+
+sc_require('models/polygon');
+
+PolygonTool.Polygon.FIXTURES = [
+
+  // TODO: Add your data fixtures here.
+  // All fixture records must have a unique primary key (default 'guid').  See 
+  // the example below.
+
+  // { guid: 1,
+  //   firstName: "Michael",
+  //   lastName: "Scott" },
+  //
+  // { guid: 2,
+  //   firstName: "Dwight",
+  //   lastName: "Schrute" },
+  //
+  // { guid: 3,
+  //   firstName: "Jim",
+  //   lastName: "Halpert" },
+  //
+  // { guid: 4,
+  //   firstName: "Pam",
+  //   lastName: "Beesly" },
+  //
+  // { guid: 5,
+  //   firstName: "Ryan",
+  //   lastName: "Howard" }
+
+];
---
+++ apps/polygon_tool/models/polygon.js
@@ -0,0 +1,19 @@
+// ==========================================================================
+// Project:   PolygonTool.Polygon
+// Copyright: ©2009 My Company, Inc.
+// ==========================================================================
+/*globals PolygonTool */
+
+/** @class
+
+  (Document your Model here)
+
+  @extends SC.Record
+  @version 0.1
+*/
+PolygonTool.Polygon = SC.Record.extend(
+/** @scope PolygonTool.Polygon.prototype */ {
+
+  // TODO: Add your own code here.
+
+}) ;

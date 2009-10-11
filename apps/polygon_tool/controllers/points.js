b2ae843fe0bb0f40ed94e4aa4630fe045ae20a61
Sam Johnson <sam@samwich.com>
2009-10-11 14:24:36 -0700

    add pointsController

---
+++ apps/polygon_tool/controllers/points.js
@@ -0,0 +1,18 @@
+// ==========================================================================
+// Project:   PolygonTool.pointsController
+// Copyright: ©2009 My Company, Inc.
+// ==========================================================================
+/*globals PolygonTool */
+
+/** @class
+
+  (Document Your Controller Here)
+
+  @extends SC.Object
+*/
+PolygonTool.pointsController = SC.ArrayController.create(
+/** @scope PolygonTool.pointsController.prototype */ {
+
+  
+
+}) ;

08ecf7a0eb156b19e5821598e93456dcebd7a738
Sam Johnson <sam@samwich.com>
2009-10-10 20:26:04 -0700

    sc-gen model PolygonTool.Point

---
+++ apps/polygon_tool/models/point.js
@@ -0,0 +1,19 @@
+// ==========================================================================
+// Project:   PolygonTool.Point
+// Copyright: ©2009 My Company, Inc.
+// ==========================================================================
+/*globals PolygonTool */
+
+/** @class
+
+  (Document your Model here)
+
+  @extends SC.Record
+  @version 0.1
+*/
+PolygonTool.Point = SC.Record.extend(
+/** @scope PolygonTool.Point.prototype */ {
+
+  // TODO: Add your own code here.
+
+}) ;
---
+++ apps/polygon_tool/tests/models/point.js
@@ -0,0 +1,15 @@
+// ==========================================================================
+// Project:   PolygonTool.Point Unit Test
+// Copyright: ©2009 My Company, Inc.
+// ==========================================================================
+/*globals PolygonTool module test ok equals same stop start */
+
+module("PolygonTool.Point");
+
+// TODO: Replace with real unit test for Point
+test("test description", function() {
+  var expected = "test";
+  var result   = "test";
+  equals(result, expected, "test should equal test");
+});
+

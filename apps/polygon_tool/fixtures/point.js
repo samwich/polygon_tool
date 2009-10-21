18ee2c6507143b0ec504c84108bb66f9e2e5bd5d
Sam Johnson <sam@samwich.com>
2009-10-20 20:43:49 -0700

    update Point fixtures

--- apps/polygon_tool/fixtures/point.js
+++ apps/polygon_tool/fixtures/point.js
@@ -9,23 +9,23 @@ sc_require('models/point');
 PolygonTool.Point.FIXTURES = [
 
   { "guid": "point-1",
-    "latitude": 40,
-    "longitude": 123 },
+    "latitude": 37.77,
+    "longitude": -122.40 },
 
   { "guid": "point-2",
-    "latitude": 41,
-    "longitude": 124 },
+    "latitude": 37.78,
+    "longitude": -122.38 },
 
   { "guid": "point-3",
-    "latitude": 42,
-    "longitude": 125 },
+    "latitude": 37.79,
+    "longitude": -122.35 },
 
   { "guid": "point-4",
-    "latitude": 43,
-    "longitude": 126 },
+    "latitude": 37.80,
+    "longitude": -122.32 },
 
   { "guid": "point-5",
-    "latitude": 44,
-    "longitude": 127 }
+    "latitude": 37.81,
+    "longitude": -122.29 }
 
 ];

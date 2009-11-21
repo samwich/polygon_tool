85c3c60ddfd58bfa82bb5ed8b8f83069f0244447
Sam Johnson <sam@samwich.com>
2009-11-20 23:32:23 -0800

    MapPointItemView

---
+++ apps/polygon_tool/views/map_point_item.js
@@ -0,0 +1,18 @@
+// ==========================================================================
+// Project:   PolygonTool.MapPointItemView
+// Copyright: ©2009 My Company, Inc.
+// ==========================================================================
+/*globals PolygonTool */
+
+/** @class
+
+  (Document Your View Here)
+
+  @extends SC.Object
+*/
+PolygonTool.MapPointItemView = SC.Object.extend(
+/** @scope PolygonTool.MapPointItemView.prototype */ {
+
+  
+
+});

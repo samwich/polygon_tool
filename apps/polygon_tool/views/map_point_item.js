65aff2166b7f9312aa0863cfa8b6e4ff570b0323
Sam Johnson <sam@samwich.com>
2009-11-22 14:56:53 -0800

    view-like stuff for points

--- apps/polygon_tool/views/map_point_item.js
+++ apps/polygon_tool/views/map_point_item.js
@@ -13,6 +13,13 @@
 PolygonTool.MapPointItemView = SC.Object.extend(
 /** @scope PolygonTool.MapPointItemView.prototype */ {
 
-  
+  // map: the gmap object - from parent,
+  // point: some point,
+
+  render: function () {
+    // create a gLatLon from point
+    // create an overlay with gLatLon
+    // add the overlay to the map
+  }
 
 });
--- apps/polygon_tool/views/map_points.js
+++ apps/polygon_tool/views/map_points.js
@@ -12,7 +12,27 @@
 */
 PolygonTool.MapPointsView = SC.Object.extend(
 /** @scope PolygonTool.MapPointsView.prototype */ {
-  // contentBindingDefault: SC.Binding.multiple()
-  contentBinding: 'PolygonTool.gMapController.points'
+  // content: null,
+  // contentBindingDefault: SC.Binding.multiple(),
+  contentBinding: 'PolygonTool.gMapController.points',
+
+  exampleView: PolygonTool.MapPointItemView,
+
+  contentIndexesInRect: function(rect) {
+    return SC.IndexSet.create(0, this.get('length'));
+  },
+
+  // contentRangeDidChange: function(content, object, key, indexes) {
+
+  contentPropertyDidChange: function(target, key, indexes) {
+    // Default Does Nothing
+  },
+
+  render: function(context, firstTime) {
+    // create an exampleView for each item in content
+    // get the list of items in content
+    // iterate through items
+    // create a new mapPointItemView for each item
+  }
 
 });
